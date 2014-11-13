703cb5509b17eb92e5077e4a6da5db39cbc8cd1d
roberto.martinmartin <roberto.martinmartin@tu-berlin.de>
2014-11-13 15:15:23 +0100

    Adding members to the Controller Class
    Added eigen include dirs in all projects

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -1,6 +1,21 @@
 #include "hybrid_automaton/Controller.h"
 
 namespace ha {
+
+	Controller::Controller()
+	{
+
+	}
+
+	Controller::~Controller()
+	{
+
+	}
+
+	Controller::Controller(const ha::Controller &controller)
+	{
+
+	}
 	
 	void Controller::serialize(DescriptionTreeNode::Ptr& tree) const {
 		// TODO
