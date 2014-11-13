08ffc978e746df2df8b636cdc0f2e93139a2c6a2
a.sieverling <arne.sieverling@tu-berlin.de>
2014-11-13 10:08:30 +0100

    Interface changes

--- include/hybrid_automaton/DescriptionTreeNodeXML.h
+++ include/hybrid_automaton/DescriptionTreeNodeXML.h
@@ -20,7 +20,11 @@ namespace ha {
 		TiXmlElement* node;
 
 	public:
-		
+
+		DescriptionTreeNodeXML(TiXmlElement* xmlNode);
+
+		virtual const std::string getType() const;
+
 		virtual bool getAttribute(const std::string& field_name, std::string& field_value) const;
 
 		/**
@@ -28,14 +32,26 @@ namespace ha {
 		* returns true, if node has at least one child of type type
 		* returns child nodes in children
 		*/
-		virtual bool getChildrenNodes(const std::string& type, const ConstNodeList& children) const;
+		virtual bool getChildrenNodes(const std::string& type, ConstNodeList& children) const;
 		
 		/**
 		* getChildrenNodes
 		* returns true, if node has at least one child of any type
 		* returns all child nodes
 		*/
-		virtual bool getChildrenNodes(const ConstNodeList& children) const;
+		virtual bool getChildrenNodes(ConstNodeList& children) const;
+
+		/**
+		* setAttribute 
+		* @param field_name returns string value of field field_name in field_value
+		*/
+		virtual void setAttribute(const std::string& field_name, std::string& field_value);
+		
+		/**
+		* setAttribute 
+		* @param field_name returns string value of field field_name in field_value
+		*/
+		virtual void addChildNode(DescriptionTreeNode& child);
 
 	};
 
