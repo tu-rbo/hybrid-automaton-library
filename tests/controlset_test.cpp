fa0bec48124e12d31570e3eea0f36a92ba35e8a3
shoefer <sebastian.hoefer@tu-berlin.de>
2014-11-17 16:35:28 +0100

    added test for ControlSet serialization
    (failing because ControlSet serialize has not been implemented yet)

--- tests/controlset_test.cpp
+++ tests/controlset_test.cpp
@@ -3,8 +3,10 @@
 
 #include "hybrid_automaton/HybridAutomaton.h"
 
+#include "hybrid_automaton/tests/MockDescriptionTree.h"
 #include "hybrid_automaton/tests/MockDescriptionTreeNode.h"
 
+#include "hybrid_automaton/Controller.h"
 #include "hybrid_automaton/ControlSet.h"
 
 using namespace std;
@@ -96,5 +98,93 @@ TEST(ControlSet, UnsuccessfulRegistration) {
 	// create controller should throw an exception because
 	// FantasyNonRegisteredController was not registered
 	ASSERT_ANY_THROW( HybridAutomaton::createControlSet(mockedNodePtr, emptySystem));
+
+}
+
+//----------------------------
+
+// we have to use different namespaces because static member "initializer" would have same name, but different types
+namespace ControlSetSerialization1 {
+
+class MockSerializableController : public ha::Controller {
+	public:
+		MOCK_CONST_METHOD1(serialize, DescriptionTreeNode::Ptr (const DescriptionTree::ConstPtr& factory) );
+		MOCK_CONST_METHOD1(deserialize, void (const ha::DescriptionTreeNode::ConstPtr& tree) );
+
+		HA_CONTROLLER_INSTANCE(node, system) {
+			return Controller::Ptr(new MockSerializableController);
+		}
+};
+HA_CONTROLLER_REGISTER("MockSerializableController1", MockSerializableController);
+
+}
+
+namespace ControlSetSerialization2 {
+
+class MockSerializableController : public ha::Controller {
+	public:
+		MOCK_CONST_METHOD1(serialize, DescriptionTreeNode::Ptr (const DescriptionTree::ConstPtr& factory) );
+		MOCK_CONST_METHOD1(deserialize, void (const ha::DescriptionTreeNode::ConstPtr& tree) );
+
+		HA_CONTROLLER_INSTANCE(node, system) {
+			return Controller::Ptr(new MockSerializableController);
+		}
+};
+HA_CONTROLLER_REGISTER("MockSerializableController2", MockSerializableController);
+
+}
+
+
+
+TEST(ControlSet, Serialization) {
+
+	std::string ctrlType1("MockSerializableController1");
+	std::string ctrlType2("MockSerializableController2");
+
+	// (mocked) controller to be serialized
+	ControlSetSerialization1::MockSerializableController * _ctrl1 = new ControlSetSerialization1::MockSerializableController;
+	Controller::Ptr ctrl1(_ctrl1);
+	_ctrl1->setType(ctrlType1);
+	_ctrl1->setName("myCtrl1");
+
+	ControlSetSerialization2::MockSerializableController * _ctrl2 = new ControlSetSerialization2::MockSerializableController;
+	Controller::Ptr ctrl2(_ctrl2);
+	_ctrl2->setType(ctrlType2);
+	_ctrl2->setName("myCtrl2");
+
+	// this will be the control set node "generated" by the tree
+	MockDescriptionTreeNode* _cs_node = new MockDescriptionTreeNode;
+	DescriptionTreeNode::Ptr cs_node(_cs_node);
+
+	// this will be the controller nodes "generated" by the tree
+	MockDescriptionTreeNode* _ctrl1_node = new MockDescriptionTreeNode;
+	DescriptionTreeNode::Ptr ctrl1_node(_ctrl1_node);
+	EXPECT_CALL(*_ctrl1_node, getType()).WillRepeatedly(Return("Controller"));
+	EXPECT_CALL(*_ctrl1, serialize(_)).WillOnce(Return(ctrl1_node));
+
+	MockDescriptionTreeNode* _ctrl2_node = new MockDescriptionTreeNode;
+	DescriptionTreeNode::Ptr ctrl2_node(_ctrl2_node);
+	EXPECT_CALL(*_ctrl2_node, getType()).WillRepeatedly(Return("Controller"));
+	EXPECT_CALL(*_ctrl2, serialize(_)).WillOnce(Return(ctrl2_node));
+
+	// Mocked tree factory
+	MockDescriptionTree* _tree = new MockDescriptionTree;
+	MockDescriptionTree::Ptr tree(_tree);
+	// second call
+	EXPECT_CALL(*_cs_node, addChildNode(ctrl1_node))
+		.WillOnce(Return());
+	// first call
+	EXPECT_CALL(*_cs_node, addChildNode(ctrl1_node))
+		.WillOnce(Return());
+
+	// mocked node for control set
+	EXPECT_CALL(*_tree, createNode("ControlSet"))
+		.WillOnce(Return(cs_node));
+
+	ControlSet::Ptr cs (new ControlSet);
+	cs->appendController(ctrl1);
+	cs->appendController(ctrl2);
+
+	cs->serialize(tree);
 
 }
