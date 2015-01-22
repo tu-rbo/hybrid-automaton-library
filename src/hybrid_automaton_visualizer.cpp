c44a7dcfafccbe63b9a3fb223da2b485bd6ae6c2
shoefer <sebastian.hoefer@tu-berlin.de>
2015-01-22 11:39:31 +0100

    minor feature to hybrid_automaton_visualizer: default output file name

--- src/hybrid_automaton_visualizer.cpp
+++ src/hybrid_automaton_visualizer.cpp
@@ -49,24 +49,40 @@ namespace ha {
 
 int main(int argc, char *argv[]) {
 
-    if (argc < 3) {
-        HA_ERROR("hybrid_automaton_visualizer", "Usage: ./hybrid_automaton_visualizer <xml file> <out file (without extension)>");
+    if (argc < 2) {
+        HA_ERROR("hybrid_automaton_visualizer", "Usage: ./hybrid_automaton_visualizer <xml file> [<out file (without extension)>]");
     }
+
+    // generate file names for output
+    std::string out_file;
+    std::string pdf_file;
+    if (argc == 3) {
+        out_file = argv[2];
+        pdf_file = argv[2];
+    } else {
+        std::string raw(argv[1]);
+        std::string ext;
+        if (raw.length() >= 4)
+            ext = raw.substr( raw.size()-3,  raw.size());
+        if (ext == "xml")
+            raw = raw.substr(0, raw.size()-4); // remove .xml
+        out_file = raw;
+        pdf_file = raw;
+    }
+    out_file += ".dot";
+    pdf_file += ".pdf";
 
     // read XML string
 
     HybridAutomaton ha;
     ha.setDeserializeDefaultEntities(true);
-    System::Ptr sys(new DummySystem);
+    //System::Ptr sys(new DummySystem);
+    System::Ptr sys; // NULL on purpose
 
     std::cout << "Reading " << argv[1] << std::endl;
     DescriptionTreeXML dt(loadXMLFile(argv[1]));
     ha.deserialize(dt.getRootNode(), sys);
 
-    std::string out_file(argv[2]);
-    std::string pdf_file(argv[2]);
-    out_file += ".dot";
-    pdf_file += ".pdf";
     ha.visualizeGraph(out_file);
 
     std::stringstream cmd;
