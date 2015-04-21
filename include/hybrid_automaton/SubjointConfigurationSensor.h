bcc504254263a0d864668d5de60d4a199d410222
sieverling <arne.sieverling@tu-berlin.de>
2015-04-21 18:26:29 +0200

    added Subjointconfigurationsensor

---
+++ include/hybrid_automaton/SubjointConfigurationSensor.h
@@ -0,0 +1,59 @@
+#ifndef SUBJOINT_CONFIGURATION_SENSOR_H
+#define SUBJOINT_CONFIGURATION_SENSOR_H
+
+#include "hybrid_automaton/Sensor.h"
+#include "hybrid_automaton/HybridAutomaton.h"
+
+#include <boost/shared_ptr.hpp>
+
+namespace ha {
+
+    class SubjointConfigurationSensor;
+    typedef boost::shared_ptr<SubjointConfigurationSensor> SubjointConfigurationSensorPtr;
+    typedef boost::shared_ptr<const SubjointConfigurationSensor> SubjointConfigurationSensorConstPtr;
+
+    class SubjointConfigurationSensor : public Sensor
+	{
+	public:
+
+        typedef boost::shared_ptr<SubjointConfigurationSensor> Ptr;
+        typedef boost::shared_ptr<const SubjointConfigurationSensor> ConstPtr;
+
+        SubjointConfigurationSensor();
+
+        virtual ~SubjointConfigurationSensor();
+
+        SubjointConfigurationSensor(const SubjointConfigurationSensor& ss);
+
+        SubjointConfigurationSensorPtr clone() const
+		{
+            return (SubjointConfigurationSensorPtr(_doClone()));
+        }
+
+		virtual ::Eigen::MatrixXd getCurrentValue() const;
+
+		virtual DescriptionTreeNode::Ptr serialize(const DescriptionTree::ConstPtr& factory) const;
+
+		virtual void deserialize(const DescriptionTreeNode::ConstPtr& tree, const System::ConstPtr& system, const HybridAutomaton* ha);
+
+		// required to enable deserialization of this sensor
+		HA_SENSOR_INSTANCE(node, system, ha) {
+            Sensor::Ptr sensor(new SubjointConfigurationSensor());
+			sensor->deserialize(node, system, ha);
+			return sensor;
+		}
+
+	protected:
+        virtual SubjointConfigurationSensor* _doClone() const
+		{
+            return (new SubjointConfigurationSensor(*this));
+		}
+
+
+        std::vector<int> _index;
+
+	};
+
+}
+
+#endif
---
+++ src/SubjointConfigurationSensor.cpp
@@ -0,0 +1,62 @@
+#include "hybrid_automaton/SubjointConfigurationSensor.h"
+
+namespace ha
+{
+
+    HA_SENSOR_REGISTER("SubjointConfigurationSensor", SubjointConfigurationSensor);
+
+    SubjointConfigurationSensor::SubjointConfigurationSensor()
+	{
+	}
+
+    SubjointConfigurationSensor::~SubjointConfigurationSensor()
+	{
+	}
+
+    SubjointConfigurationSensor::SubjointConfigurationSensor(const SubjointConfigurationSensor& ss)
+        :Sensor(ss), _index(ss._index)
+	{
+	}
+
+    ::Eigen::MatrixXd SubjointConfigurationSensor::getCurrentValue() const
+	{
+        ::Eigen::MatrixXd cfg = this->_system->getConfiguration();
+        ::Eigen::MatrixXd subcfg(_index.size(), 1);
+        for(int i=0; i< _index.size(); i++)
+            subcfg(i) = cfg(_index[i]);
+
+        return subcfg;
+	}
+
+    DescriptionTreeNode::Ptr SubjointConfigurationSensor::serialize(const DescriptionTree::ConstPtr& factory) const
+	{
+		DescriptionTreeNode::Ptr tree = factory->createNode("Sensor");
+
+		tree->setAttribute<std::string>(std::string("type"), this->getType());
+
+		return tree;
+	}
+
+    void SubjointConfigurationSensor::deserialize(const DescriptionTreeNode::ConstPtr& tree, const System::ConstPtr& system, const HybridAutomaton* ha)
+	{
+		if (tree->getType() != "Sensor") {
+            HA_THROW_ERROR("SubjointConfigurationSensor.deserialize", "DescriptionTreeNode must have type 'Sensor', not '" << tree->getType() << "'!");
+		}
+		tree->getAttribute<std::string>("type", _type, "");
+
+        ::Eigen::MatrixXd index_mat;
+        if(!tree->getAttribute< ::Eigen::MatrixXd >("index", index_mat) )
+            HA_THROW_ERROR("SubjointConfigurationSensor.deserialize", "This type of sensor needs a value 'index'!");
+
+        for(int j=0; j<index_mat.rows(); j++)
+            _index.push_back(index_mat(j));
+
+		if (_type == "" || !HybridAutomaton::isSensorRegistered(_type)) {
+            HA_THROW_ERROR("SubjointConfigurationSensor.deserialize", "SensorType type '" << _type << "' "
+				<< "invalid - empty or not registered with HybridAutomaton!");
+		}
+
+		_system = system;
+	}
+
+}
